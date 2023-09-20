8888394bec8fc8adeba61a37284b39ce4a5b5eb9
yinonab <yinonabarjel@gmail.com>
2023-09-20 16:12:27 +0300

    F/add-toy-service/create the toy service

--- frontend/src/services/util.service.js
+++ frontend/src/services/util.service.js
@@ -6,7 +6,7 @@ export const utilService = {
     saveToStorage,
     animateCSS,
     debounce,
-    getAssetSrc
+    getAssetSrc,makeLabel,getTimeFromStamp
 }
 
 function makeId(length = 6) {
@@ -80,4 +80,86 @@ function getAssetSrc(name) {
     const modules = import.meta.globEager('/src/assets/img/*')
     const mod = modules[path]
     return mod.default
+}
+
+function getTimeFromStamp(timestamp) {
+    const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ]
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
+    ]
+    const date = new Date(timestamp)
+    const time =
+        timeFormat(date.getHours()) + ':' + timeFormat(date.getMinutes())
+    const currTimestamp = Date.now()
+    const currDate = new Date(currTimestamp)
+    const day = 1000 * 60 * 60 * 24
+    if (currTimestamp - timestamp < day) return 'Today ' + time
+    if (currTimestamp - timestamp < day * 2) return 'Yesterday ' + time
+    if (currTimestamp - timestamp < day * 7) return days[date.getDay()]
+    if (currDate.getUTCFullYear() !== date.getUTCFullYear())
+        return months[date.getMonth()].slice(0, 3) + ' ' + date.getUTCFullYear()
+    return date.getDate() + ' ' + months[date.getMonth()].slice(0, 3)
+}
+
+function timeFormat(time) {
+    return time < 10 ? '0' + time : time
+}
+function debounce(fn, wait) {
+    let timer
+    return function (...args) {
+        if (timer) {
+            clearTimeout(timer) // clear any pre-existing timer
+        }
+        const context = this // get the current context
+        timer = setTimeout(() => {
+            fn.apply(context, args) // call the function if time expires
+        }, wait)
+    }
+}
+function makeLorem(size = 1) {
+    var words = ['sky', 'above', 'port', 'was', 'color ', 'tuned', 'to', ' channel', 'All', ' happened', 'more', 'I', 'had', 'story', 'bit', 'various', 'and', 'generally', 'happens', 'cases', 'time', 'it', 'was', 'story', 'It', 'was', 'pleasure', 'to', 'burn']
+    var txt = ''
+    while (size > 0) {
+        size--
+        txt += words[Math.floor(Math.random() * words.length)] + ''
+    }
+    return txt
+}
+function makeLabel(size = 3) {
+    var words = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle', 'Outdoor', 'Battery Powered']
+    var txt = ''
+    while (size > 0) {
+        size--
+        txt += words[Math.floor(Math.random() * words.length)] + ''
+    }
+    return txt
+}
+function makePhoneNumber(length = 8) {
+    var txt = ''
+    var possible = '0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
 }
