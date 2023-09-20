b3118e48ecf38998fa037a0bf50534a29159b1ac
itamarsa <itamarr87@gmail.com>
2023-09-20 17:30:50 +0300

    F/create-list-cmp/added list cmp

---
+++ frontend/src/cmps/ToyList.jsx
@@ -0,0 +1,18 @@
+// import { ToyPreview } from "./ToyPreview.jsx"
+
+export function ToyList({ toys, onRemoveToy, onEditToy }) {
+
+    return (
+        <ul className="toy-list">
+            {toys.map(toy =>
+                <li className="toy-preview" key={toy._id}>
+                    {/* <ToyPreview toy={toy} /> */}
+                    <div>
+                        <button onClick={() => onRemoveToy(toy._id)}>x</button>
+                        <button onClick={() => onEditToy(toy)}>Edit</button>
+                    </div>
+                </li>
+            )}
+        </ul>
+    )
+}
