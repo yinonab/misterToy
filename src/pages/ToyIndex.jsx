45d37d04ad741382897c0a12722f7887c2b2f53b
yinonab <yinonabarjel@gmail.com>
2023-09-22 13:46:58 +0300

    F/design for the sort buttons

--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -76,12 +76,6 @@ export function ToyIndex() {
             <h3 className="custom-h3">Toys App</h3>
             <main className="custom-main">
                 <ToyFilter filterBy={filterBy} onSetFilter={onSetFilter} />
-                <button className="btn-add-toy" onClick={onAddToy}>add Toy 🧸</button>
-                <ToyList
-                    toys={displayedToys}
-                    onRemoveToy={onRemoveToy}
-                    onEditToy={onEditToy}
-                />
                 <ReactPaginate
                     previousLabel={'Previous'}
                     nextLabel={'Next'}
@@ -89,6 +83,12 @@ export function ToyIndex() {
                     onPageChange={({ selected }) => setCurrentPage(selected)}
                     containerClassName={'pagination'}
                     activeClassName={'active'}
+                />
+                <button className="btn-add-toy" onClick={onAddToy}>add Toy 🧸</button>
+                <ToyList
+                    toys={displayedToys}
+                    onRemoveToy={onRemoveToy}
+                    onEditToy={onEditToy}
                 />
             </main>
         </div>
