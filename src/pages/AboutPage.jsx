5fc4f64981105fa2b10d20b6589c1ac0dd237c32
Ozhodeda <oz36002@gmail.com>
2023-09-22 09:24:52 +0300

    F/create-about-page/add details for about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -13,26 +13,37 @@ export function AboutPage() {
         setCoordinates({ lat, lng })
     }
 
-    function goToLocation(target){
-        const id=target.target.id
-        if(id==='rehovot'){
-        handleClick({lat: 31.894756, lng:34.809322})
+    function goToLocation(target) {
+        const id = target.target.id
+        if (id === 'rehovot') {
+            handleClick({ lat: 31.894756, lng: 34.809322 })
         }
-        if(id==='tel aviv'){
+        if (id === 'tel aviv') {
             handleClick({ lat: 32.0853, lng: 34.7818 })
         }
-        if(id==='hadera'){
-            handleClick({ lat:32.43983, lng: 34.91744 })
+        if (id === 'hadera') {
+            handleClick({ lat: 32.43983, lng: 34.91744 })
         }
-        if(id==='bat yam'){
-            handleClick({ lat:32.02379, lng:34.75185})
+        if (id === 'bat yam') {
+            handleClick({ lat: 32.02379, lng: 34.75185 })
         }
-        
+
     }
 
     return (
 
         <section>
+            <h1 className="about-title">
+                Welcome to toy store
+            </h1>
+            <p className="about-p">
+                The store was created as part of the bootcamp by 3 students
+                <li>Oz Hodeda</li>
+                <li>Yinon Abarjel</li>
+                <li>Itamar Sasson</li>
+                in the store you can find any toys tou want <br />
+                you can filter them sort them and see any details about the toys 
+            </p>
             <div style={{ height: '70vh', width: '70%' }}>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: API_KEY }}
@@ -46,9 +57,18 @@ export function AboutPage() {
                     />
                 </GoogleMapReact>
             </div>
-            <button id='tel aviv' onClick={goToLocation}>Tel-Aviv</button>
-            <button id='hadera' onClick={goToLocation}>Hadera</button>
-            <button id='bat yam' onClick={goToLocation}> Bat-Yam</button>
+            <ul className="btn-branches">
+                <h3 className="branches-title">branches</h3>
+                <li>
+                    <button id='tel aviv' onClick={goToLocation}>Tel-Aviv</button>
+                </li>
+                <li>
+                    <button id='hadera' onClick={goToLocation}>Hadera</button>
+                </li>
+                <li>
+                    <button id='bat yam' onClick={goToLocation}> Bat-Yam</button>
+                </li>
+            </ul>
         </section>
     )
 }
